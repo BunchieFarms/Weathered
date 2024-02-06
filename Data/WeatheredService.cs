794100e40b15c49d1984c8c29a1e9b6001bd254a
Bryce <bryceohmer@hotmail.com>
2024-02-06 00:06:08 -0500

    Moved data models out of WeatheredService, added method and modified class contructor to get the datetime of the searched location

--- Data/WeatheredService.cs
+++ Data/WeatheredService.cs
@@ -4,8 +4,9 @@ using GoogleApi.Entities.Maps.Geocoding;
 using Microsoft.EntityFrameworkCore;
 using Weathered.Models;
 using GoogleApi.Entities.Common;
-using System.Globalization;
-using System.Text.Json;
+using GoogleApi.Entities.Common.Extensions;
+using GoogleApi.Entities.Maps.TimeZone.Request;
+using GoogleApi.Entities.Maps.TimeZone.Response;
 
 namespace Weathered.Data
 {
@@ -14,13 +15,15 @@ namespace Weathered.Data
         private readonly IConfiguration _config;
         private readonly WeatheredContext _context;
         private readonly GoogleMaps.Geocode.AddressGeocodeApi _geocodeApi;
+        private readonly GoogleMaps.TimeZoneApi _timezoneApi;
         private readonly HttpClient _client;
-        public WeatheredService(WeatheredContext context, GoogleMaps.Geocode.AddressGeocodeApi geocodeApi, IConfiguration config, HttpClient client)
+        public WeatheredService(WeatheredContext context, GoogleMaps.Geocode.AddressGeocodeApi geocodeApi, IConfiguration config, HttpClient client, GoogleMaps.TimeZoneApi timezoneApi)
         {
             _context = context;
             _geocodeApi = geocodeApi;
             _config = config;
             _client = client;
+            _timezoneApi = timezoneApi;
         }
         public async Task<WeatheredResponse> GetWeatheredResponse(string location)
         {
@@ -33,19 +36,12 @@ namespace Weathered.Data
             if (resResults == null)
                 return pastWeatherResponse;
 
-            StationLoc nearestStation = GetNearestStation(resResults.Geometry.Location);
-            pastWeatherResponse = new WeatheredResponse(nearestStation);
+            var locationTime = await GetLocationTime(resResults.Geometry.Location);
+            DateTime locationDateTime = DateTimeExtension.epoch.AddSeconds((double)(DateTime.UtcNow.DateTimeToUnixTimestamp() + locationTime.RawOffSet + locationTime.OffSet));
 
-            //List<DaySummary> forecastDaySummaries = pastWeatherResponse.DaySummaries.Where(x => x.isForecast).ToList();
-            //if (forecastDaySummaries.Count > 0)
-            //{
-            //    var histForecast = GetHistoricalForecast(nearestStation.Lat, nearestStation.Lon, forecastDaySummaries[0].Date);
-            //    for (var i = 0; i < forecastDaySummaries.Count; i++)
-            //    {
-            //        pastWeatherResponse.DaySummaries.First(x => x.Date == forecastDaySummaries[i].Date).Temp = histForecast[i].temperatureMax;
-            //        pastWeatherResponse.DaySummaries.First(x => x.Date == forecastDaySummaries[i].Date).Prcp = histForecast[i].precipAccumulation;
-            //    }
-            //}
+            StationLoc nearestStation = GetNearestStation(resResults.Geometry.Location);
+            pastWeatherResponse = new WeatheredResponse(nearestStation, locationDateTime);
+
 
             return pastWeatherResponse;
         }
@@ -57,6 +53,13 @@ namespace Weathered.Data
             var apiResponse = _client.GetFromJsonAsync<PirateForecastResponse>(reqStr);
             return apiResponse.Result.daily.data;
         }
+
+        public async Task<TimeZoneResponse> GetLocationTime(Coordinate coords)
+        {
+            TimeZoneRequest tzReq = new TimeZoneRequest { Key = _config["GeocodingKey"], Location = coords, TimeStamp = DateTime.UtcNow };
+            return await _timezoneApi.QueryAsync(tzReq);
+        }
+
 
         private StationLoc GetNearestStation(Coordinate coords)
         {
@@ -88,91 +91,4 @@ namespace Weathered.Data
                             x.Lon >= coords.Longitude - size && x.Lon <= coords.Longitude + size).ToList();
         }
     }
-
-
-    public class PirateForecastResponse
-    {
-        public float latitude { get; set; }
-        public float longitude { get; set; }
-        public Daily daily { get; set; }
-    }
-
-    public class Daily
-    {
-        public string summary { get; set; }
-        public string icon { get; set; }
-        public Datum[] data { get; set; }
-    }
-
-    public class Datum
-    {
-        public int time { get; set; }
-        public string icon { get; set; }
-        public string summary { get; set; }
-        public float precipAccumulation { get; set; }
-        public string precipType { get; set; }
-        public float temperatureHigh { get; set; }
-        public float temperatureLow { get; set; }
-        public float apparentTemperatureHigh { get; set; }
-        public float apparentTemperatureLow { get; set; }
-        public float temperatureMin { get; set; }
-        public int temperatureMinTime { get; set; }
-        public float temperatureMax { get; set; }
-        public int temperatureMaxTime { get; set; }
-        public float apparentTemperatureMin { get; set; }
-        public int apparentTemperatureMinTime { get; set; }
-        public float apparentTemperatureMax { get; set; }
-        public int apparentTemperatureMaxTime { get; set; }
-    }
-
-
-    public class WeatheredResponse
-    {
-        public readonly string StationName;
-        public readonly int StationLocId;
-        public readonly float Lat;
-        public readonly float Lon;
-        public readonly List<DaySummary> DaySummaries = new List<DaySummary>();
-        public WeatheredResponse(StationLoc station)
-        {
-            StationName = station.Name;
-            StationLocId = station.StationLocId;
-            Lat = station.Lat;
-            Lon = station.Lon;
-
-            var tempData = Array.ConvertAll(station.PastWeekData.TempData.Split(','), float.Parse);
-            var prcpData = Array.ConvertAll(station.PastWeekData.PrecipData.Split(','), float.Parse);
-            var daysAgo = -7;
-
-            for (int i = 0; i < 7; i++)
-            {
-                DaySummaries?.Add(new DaySummary(DateTime.Now.AddDays(daysAgo), prcpData[i], tempData[i]));
-                daysAgo++;
-            }
-        }
-        public WeatheredResponse()
-        {
-            StationName = "Geocoding Failed.";
-            StationLocId = -1;
-            Lat = -1;
-            Lon = -1;
-            DaySummaries = new List<DaySummary>();
-        }
-    }
-
-    public class DaySummary
-    {
-        public readonly DateOnly Date;
-        public float Prcp;
-        public float Temp;
-        public readonly bool isForecast;
-        public DaySummary(DateTime _date, float _prcp, float _temp)
-        {
-            Date = DateOnly.FromDateTime(_date);
-            Prcp = _prcp;
-            Temp = _temp;
-            isForecast = _temp == 9999.9f || _prcp == 99.99f;
-        }
-    }
-
 }
---
+++ Models/Non-EF Models/PirateForecaseResponse.cs
@@ -0,0 +1,37 @@
+﻿namespace Weathered.Models
+{
+    public class PirateForecastResponse
+    {
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public Daily daily { get; set; }
+    }
+
+    public class Daily
+    {
+        public string summary { get; set; }
+        public string icon { get; set; }
+        public Datum[] data { get; set; }
+    }
+
+    public class Datum
+    {
+        public int time { get; set; }
+        public string icon { get; set; }
+        public string summary { get; set; }
+        public float precipAccumulation { get; set; }
+        public string precipType { get; set; }
+        public float temperatureHigh { get; set; }
+        public float temperatureLow { get; set; }
+        public float apparentTemperatureHigh { get; set; }
+        public float apparentTemperatureLow { get; set; }
+        public float temperatureMin { get; set; }
+        public int temperatureMinTime { get; set; }
+        public float temperatureMax { get; set; }
+        public int temperatureMaxTime { get; set; }
+        public float apparentTemperatureMin { get; set; }
+        public int apparentTemperatureMinTime { get; set; }
+        public float apparentTemperatureMax { get; set; }
+        public int apparentTemperatureMaxTime { get; set; }
+    }
+}
