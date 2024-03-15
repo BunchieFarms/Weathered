df1e5a082b38e1587dd85ba80fef6444c7779abc
Bryce <bryceohmer@hotmail.com>
2024-03-15 12:55:58 -0400

    Added dark mode, saved to local storage

--- Weathered-Web/Program.cs
+++ Weathered-Web/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -14,5 +15,6 @@ string baseAddress = "http://weathered.bryceohmer.com";
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
